f23d6c7f74de52d35db94c5bfcf8e951a0d2eb0e
Gabriel Ruivo <gabrielruivo@Gabriels-MacBook-Pro.local>
2020-12-26 15:09:10 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333',
+});
+class App extends Component {
+
+  state = {
+    newPostContent: '',
+    posts: [],
+  };
+
+  async componentDidMount() {
+
+    try {
+      const { data: posts } = await api.get('/posts');
+      this.setState({ posts });
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  handlePostSave = async (e) => {
+    e.preventDefault()
+
+    const { data: post } = await api.post('/posts', {
+      content: this.state.newPostContent
+    })
+
+    this.setState({ 
+      posts: [...this.state.posts, post], 
+      newPostContent: '' 
+    });
+  }
+
+  handleDelete = async (id) => {
+    await api.delete(`/posts/${id}`)
+
+    this.setState({ posts: this.state.posts.filter(item => item.id !== id) })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <form onSubmit={this.handlePostSave} >
+          <textarea 
+            onChange={e => this.setState({ newPostContent: e.target.value })} 
+            value={this.state.newPostContent}
+          />
+          <button type="submit" >Postar</button>
+        </form>
+        <ul>
+          {this.state.posts.map(post => (
+              <li key={post.id} >{post.content}
+                <button onClick={ () => this.handleDelete(post.id)} >Del</button>
+              </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
